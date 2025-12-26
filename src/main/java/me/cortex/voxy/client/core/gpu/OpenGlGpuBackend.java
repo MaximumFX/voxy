5e7887ca10c6f5d3b69814430d19e45cc8e2cbe3
MaximumFX <max@maximumfx.nl>
2025-12-27 00:06:31 +0100

    feat: implement full OpenGL GPU backend capabilities in GpuCommandList

--- src/main/java/me/cortex/voxy/client/core/gpu/OpenGlGpuBackend.java
+++ src/main/java/me/cortex/voxy/client/core/gpu/OpenGlGpuBackend.java
@@ -5,6 +5,30 @@ import me.cortex.voxy.client.core.gl.GlFence;
 import me.cortex.voxy.client.core.gl.GlPersistentMappedBuffer;
 import me.cortex.voxy.client.core.gl.GlTexture;
 
+import static org.lwjgl.opengl.GL11C.glEnable;
+import static org.lwjgl.opengl.GL11C.glDisable;
+import static org.lwjgl.opengl.GL11C.glDepthFunc;
+import static org.lwjgl.opengl.GL11C.glStencilFunc;
+import static org.lwjgl.opengl.GL11C.glStencilMask;
+import static org.lwjgl.opengl.GL11C.glStencilOp;
+import static org.lwjgl.opengl.GL11C.glColorMask;
+import static org.lwjgl.opengl.GL14.glBlendFuncSeparate;
+import static org.lwjgl.opengl.GL30C.GL_FRAMEBUFFER;
+import static org.lwjgl.opengl.GL30C.glBindFramebuffer;
+import static org.lwjgl.opengl.GL33C.glBindSampler;
+import static org.lwjgl.opengl.GL42.glMemoryBarrier;
+import static org.lwjgl.opengl.GL42C.glDrawArraysInstancedBaseInstance;
+import static org.lwjgl.opengl.GL42C.glDrawElementsInstancedBaseVertexBaseInstance;
+import static org.lwjgl.opengl.GL43.glBindImageTexture;
+import static org.lwjgl.opengl.GL45.glClearNamedFramebufferfi;
+import static org.lwjgl.opengl.GL45.glGetNamedFramebufferAttachmentParameteri;
+import static org.lwjgl.opengl.GL45C.glBindTextureUnit;
+import static org.lwjgl.opengl.GL45C.glTextureParameterf;
+import static org.lwjgl.opengl.GL45C.nglUniformMatrix4fv;
+import static org.lwjgl.opengl.GL45C.glUniform2f;
+import static org.lwjgl.opengl.GL45C.glUniform4f;
+import static org.lwjgl.opengl.ARBComputeShader.glDispatchCompute;
+
 final class OpenGlGpuBackend implements GpuBackend {
     private final GpuDevice device = new OpenGlGpuDevice();
 
@@ -92,6 +116,131 @@ final class OpenGlGpuBackend implements GpuBackend {
 
         @Override
         public void end() {
+        }
+
+        @Override
+        public void beginRenderPass(int framebufferId) {
+            glBindFramebuffer(GL_FRAMEBUFFER, framebufferId);
+        }
+
+        @Override
+        public void endRenderPass() {
+        }
+
+        @Override
+        public void bindPipeline(GpuPipeline pipeline) {
+        }
+
+        @Override
+        public void bindDescriptors(int firstUnit, int... textureIds) {
+            for (int i = 0; i < textureIds.length; i++) {
+                glBindTextureUnit(firstUnit + i, textureIds[i]);
+            }
+        }
+
+        @Override
+        public void bindImageTexture(int unit, int textureId, int level, boolean layered, int layer, int access, int format) {
+            glBindImageTexture(unit, textureId, level, layered, layer, access, format);
+        }
+
+        @Override
+        public void bindSampler(int unit, int samplerId) {
+            glBindSampler(unit, samplerId);
+        }
+
+        @Override
+        public void draw(int mode, int vertexCount, int instanceCount, int firstVertex, int firstInstance) {
+            glDrawArraysInstancedBaseInstance(mode, firstVertex, vertexCount, instanceCount, firstInstance);
+        }
+
+        @Override
+        public void drawIndexed(int mode, int indexCount, int indexType, long indicesOffset, int instanceCount, int baseVertex, int baseInstance) {
+            glDrawElementsInstancedBaseVertexBaseInstance(mode, indexCount, indexType, indicesOffset, instanceCount, baseVertex, baseInstance);
+        }
+
+        @Override
+        public void dispatchCompute(int groupCountX, int groupCountY, int groupCountZ) {
+            glDispatchCompute(groupCountX, groupCountY, groupCountZ);
+        }
+
+        @Override
+        public void bufferBarrier(int barriers) {
+            glMemoryBarrier(barriers);
+        }
+
+        @Override
+        public void imageBarrier(int barriers) {
+            glMemoryBarrier(barriers);
+        }
+
+        @Override
+        public void setUniformMatrix4fv(int location, int count, boolean transpose, long value) {
+            nglUniformMatrix4fv(location, count, transpose, value);
+        }
+
+        @Override
+        public void setUniform4f(int location, float v0, float v1, float v2, float v3) {
+            glUniform4f(location, v0, v1, v2, v3);
+        }
+
+        @Override
+        public void setUniform2f(int location, float v0, float v1) {
+            glUniform2f(location, v0, v1);
+        }
+
+        @Override
+        public void setColorMask(boolean red, boolean green, boolean blue, boolean alpha) {
+            glColorMask(red, green, blue, alpha);
+        }
+
+        @Override
+        public void enable(int cap) {
+            glEnable(cap);
+        }
+
+        @Override
+        public void disable(int cap) {
+            glDisable(cap);
+        }
+
+        @Override
+        public void blendFuncSeparate(int srcRgb, int dstRgb, int srcAlpha, int dstAlpha) {
+            glBlendFuncSeparate(srcRgb, dstRgb, srcAlpha, dstAlpha);
+        }
+
+        @Override
+        public void depthFunc(int func) {
+            glDepthFunc(func);
+        }
+
+        @Override
+        public void stencilOp(int sfail, int dpfail, int dppass) {
+            glStencilOp(sfail, dpfail, dppass);
+        }
+
+        @Override
+        public void stencilFunc(int func, int ref, int mask) {
+            glStencilFunc(func, ref, mask);
+        }
+
+        @Override
+        public void stencilMask(int mask) {
+            glStencilMask(mask);
+        }
+
+        @Override
+        public void clearNamedFramebufferfi(int framebuffer, int buffer, int drawbuffer, float depth, int stencil) {
+            glClearNamedFramebufferfi(framebuffer, buffer, drawbuffer, depth, stencil);
+        }
+
+        @Override
+        public int getNamedFramebufferAttachmentParameteri(int framebuffer, int attachment, int pname) {
+            return glGetNamedFramebufferAttachmentParameteri(framebuffer, attachment, pname);
+        }
+
+        @Override
+        public void textureParameterf(int texture, int pname, float param) {
+            glTextureParameterf(texture, pname, param);
         }
     }
 }
