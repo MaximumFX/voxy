4353f27fa1bcbdf2a6ddcaefb127947873cb1a3e
MaximumFX <max@maximumfx.nl>
2025-12-27 15:48:04 +0100

    feat: add signaling and waiting mechanism to GpuFence with thread-safe latch

--- src/main/java/me/cortex/voxy/client/core/gpu/GpuFence.java
+++ src/main/java/me/cortex/voxy/client/core/gpu/GpuFence.java
@@ -3,6 +3,15 @@ package me.cortex.voxy.client.core.gpu;
 public interface GpuFence extends AutoCloseable {
     boolean signaled();
 
+    default void signal() {
+    }
+
+    default void await() {
+        while (!signaled()) {
+            Thread.onSpinWait();
+        }
+    }
+
     default void free() {
         close();
     }
--- src/main/java/me/cortex/voxy/client/core/gpu/VulkanGpuFence.java
+++ src/main/java/me/cortex/voxy/client/core/gpu/VulkanGpuFence.java
@@ -1,12 +1,16 @@
 package me.cortex.voxy.client.core.gpu;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 final class VulkanGpuFence implements GpuFence {
     private final AtomicBoolean signaled = new AtomicBoolean(false);
+    private final CountDownLatch latch = new CountDownLatch(1);
 
-    void signal() {
-        this.signaled.set(true);
+    void signalInternal() {
+        if (this.signaled.compareAndSet(false, true)) {
+            this.latch.countDown();
+        }
     }
 
     @Override
@@ -15,8 +19,25 @@ final class VulkanGpuFence implements GpuFence {
     }
 
     @Override
+    public void signal() {
+        signalInternal();
+    }
+
+    @Override
+    public void await() {
+        if (this.signaled.get()) {
+            return;
+        }
+        try {
+            this.latch.await();
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    @Override
     public void close() {
-        signal();
+        signalInternal();
     }
 
     @Override
